7c87b0264ece17c55e97dd1d9c9da00056e49f1e
Zhxiy <19845416178@163.com>
2021-12-02 15:23:14 +0800

    完成了主页相关开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,17 @@
 jQuery(function () {
     // 获取用户基本信息
     getUserData()
+    // 退出功能
+    var layer = layui.layer
+    jQuery('#Exit').on('click', function () {
+
+        layer.confirm('你确定要退出码?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            localStorage.removeItem('token')
+            location.href = "/login.html"
+            layer.close(index);
+        });
+    })
 })
 // 获取用户基本信息
 function getUserData() {
@@ -11,8 +22,25 @@ function getUserData() {
             Authorization: localStorage.getItem('token')
         },
         success: function (res) {
-            console.log(res);
-            // console.log(localStorage.getItem('token'));
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            }
+            // console.log(res);
+            // 渲染用户头像
+            rederA(res.data)
         }
     })
+}
+// 渲染用户头像
+function rederA(user) {
+    var name = user.nickname || user.username
+    jQuery('.welcome').html('欢迎&nbsp;&nbsp;' + name)
+    if (user.user_pic === null) {
+        var first = name[0].toUpperCase()
+        jQuery('.text-a').html(first).show()
+        jQuery('.Aaa').hide()
+    } else {
+        jQuery('.Aaa').attr('src', user.user_pic)
+        jQuery('text-a').hide()
+    }
 }
